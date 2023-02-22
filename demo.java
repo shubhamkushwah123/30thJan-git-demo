d10cb0b53809c09b3af53477ab4b0daaea1f0d74
Shubham <shubhamkushwah123@gmail.com>
2023-02-22 15:45:38 +0000

    DevOps Chages commited

--- demo.java
+++ demo.java
@@ -2,5 +2,7 @@ public class Demo{
 	public static void main(Strig args[]){
 		//print statement for hello world
 		System.out.println("Hello World");
+		//print statement for DevOps
+		System.out.println("Welcome to DevOps");
 	}
 }
