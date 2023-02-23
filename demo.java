99a6f25376270c19023fb687c1f4015248bdbbc3
Shubham Singh Kushwah <shubhamkushwah123@gmail.com>
2023-02-23 22:10:51 +0530

    Update demo.java
    modified demo.java on remote

--- demo.java
+++ demo.java
@@ -1,7 +1,7 @@
 public class Demo{
 	public static void main(Strig args[]){
-		//DevB code for Hello World
-		System.out.println("Hello World");
+		
+		System.out.println("modified demo.java on remote");
 		//print statement for DevOps
 		System.out.println("Welcome to DevOps");
 	}
