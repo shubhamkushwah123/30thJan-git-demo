f02500f34d13dc91f0c1080c04a252df118b6a9a
Shubham Singh Kushwah <shubhamkushwah123@gmail.com>
2023-02-23 22:16:26 +0530

    conflicts resolved

--- demo.java
+++ demo.java
@@ -1,6 +1,6 @@
 public class Demo{
 	public static void main(Strig args[]){
-		
+
 		System.out.println("modified demo.java on remote");
 		//print statement for DevOps
 		System.out.println("Welcome to DevOps");
